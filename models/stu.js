70dd45ade30eec2bff3f1469a5b249adbdd8fe1b
gurushishya <68097454+gurushishya@users.noreply.github.com>
2020-07-10 12:51:07 +0530

    Update stu.js

--- models/stu.js
+++ models/stu.js
@@ -2,7 +2,10 @@ var mongoose=require("mongoose");
 var passportlocalmongoose=require("passport-local-mongoose");
 var stuschema = new mongoose.Schema({
     Student_id: String,
-	password : String
+    Name:String,
+    phone:Number,
+    class_code:String,
+    password : String
 });
 stuschema.plugin(passportlocalmongoose);
 module.exports = mongoose.model("stu",stuschema);
